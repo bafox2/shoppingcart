0062be1eed52477c6d68cab44154a27dc04e4c43
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-05-26 16:09:01 -0400

    starting router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "../css/Home.module.css";
 // import landingPic from "../images/rtx-3090.jpg";
 
@@ -7,18 +6,7 @@ const Home = (props) => {
     return (
         <div className={styles.container}>
             <main>
-                <section
-                    // style={{ backgroundImage: `url(${landingPic})` }}
-                    className={styles.background}
-                >
-                    <h1 className={styles.s1Title}>Welcome home</h1>
-                    <p className={styles.s1Subtitle}>
-                        Now available for preorder, shipping December, 2020.
-                    </p>
-                    <Link className={styles.s1Link} to="/shopping/allItems">
-                        Shop now
-                    </Link>
-                </section>
+
             </main>
         </div>
     );
