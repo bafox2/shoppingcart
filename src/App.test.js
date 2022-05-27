7f461fd77a952e808d973160077aa594def98104
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-05-27 18:14:40 -0400

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,67 +2,71 @@ import { render, screen } from '@testing-library/react';
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 //import Searchbox from components
 
-//this will mock the items         uses model https://github.com/TheOdinProject/theodinproject/blob/main/app/javascript/components/project-submissions/components/__tests__/submissions-list.test.jsx
-jest.mock('../items', () => ({ item, isDashboardView }) => (
-  <>
-    <div data-test-id="item">{item.id}</div>
-    <div data-test-id="dashboard">{isDashboardView.toString()}</div>
-  </>
-));
-
-test('renders header across all pages', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 test('renders clicking link takes you to right spot', () => {
-  render(<App />);
-  const button = screen.getByRole("button", { name: "Click Me" });
-  userEvent.click(button);
-  expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
-
+  render(<MemoryRouter>
+    <App />
+  </MemoryRouter >);
+  const link = screen.getByRole("Link", { name: "Shop" });
+  console.log(link)
+  userEvent.click(link);
+  expect(screen.getByRole("heading").textContent).toMatch(/Weoc9me to shop/i);
 });
 
-test('correct number of items render', () => {
-  //use the mock thing like from above
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// //this will mock the items         uses model https://github.com/TheOdinProject/theodinproject/blob/main/app/javascript/components/project-submissions/components/__tests__/submissions-list.test.jsx
+// jest.mock('../items', () => ({ item, isDashboardView }) => (
+//   <>
+//     <div data-test-id="item">{item.id}</div>
+//     <div data-test-id="dashboard">{isDashboardView.toString()}</div>
+//   </>
+// ));
 
-test('item renders with correct header', () => {
-  //will need some type of mocking 
-  render(<App />);
-  const button = screen.getByRole("button", { name: "Click Me" });
-  userEvent.click(button);
-  expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+// test('renders header across all pages', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-});
+// test('correct number of items render', () => {
+//   //use the mock thing like from above
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('shopping cart total works', () => {
-  //will need some type of mocking 
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('item renders with correct header', () => {
+//   //will need some type of mocking 
+//   render(<App />);
+//   const button = screen.getByRole("button", { name: "Click Me" });
+//   userEvent.click(button);
+//   expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+
+// });
+
+// test('shopping cart total works', () => {
+//   //will need some type of mocking 
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 
-test("input has correct values", () => {
-  const onChangeMock = jest.fn();
-  // render(<Header onChange={onChangeMock} />);
-  const input = screen.getByRole("textbox");
-  userEvent.type(input, "Whale");
+// test("input has correct values", () => {
+//   const onChangeMock = jest.fn();
+//   // render(<Header onChange={onChangeMock} />);
+//   const input = screen.getByRole("textbox");
+//   userEvent.type(input, "Whale");
 
-  expect(input).toHaveValue("Whale");
-});
+//   expect(input).toHaveValue("Whale");
+// });
 
 
-test('increment/decrement works', () => {
-  //will need some type of mocking can combine with a previous test 
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('increment/decrement works', () => {
+//   //will need some type of mocking can combine with a previous test 
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
