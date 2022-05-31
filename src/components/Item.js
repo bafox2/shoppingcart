80c749db950fccadead58267105ea1057350d9eb
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-05-31 17:59:06 -0400

    starting to fill out items

---
+++ src/components/Item.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "../css/Item.module.css";
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+    return (
+        <card >
+            <Link to={`/${props.category}`}><p>Back to {props.category}</p></Link>
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <p>{props.amount} for</p>
+            <p>{props.price}</p>
+            <img alt='item ' href={props.picture} />
+        </card>
+    );
+};
+
+export default Header;
