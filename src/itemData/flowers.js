162315e3f60681cff4f072b39765f18f26e30125
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-05-31 22:01:54 -0400

    added item objects and pics

--- src/itemData/flowers.js
+++ src/itemData/flowers.js
@@ -1,19 +1,35 @@
 const flowers = [
     {
-        name: 'tomatoes',
+        name: 'Blueberry Flower',
         category: 'traditionalproduce',
         amount: '1 pound',
         price: '$1/pound',
         description: 'Seed from Mayan origin',
-        image: 'https://photos.app.goo.gl/1BLDCQV6bJBQWPeu9',
+        image: 'https://photos.app.goo.gl/wr9vkUs4cwH9uJhh8',
     },
     {
-        name: 'tomatoes',
+        name: 'Dianthus',
         category: 'traditionalproduce',
         amount: '1 pound',
         price: '$1/pound',
         description: 'Seed from Mayan origin',
-        image: 'https://photos.app.goo.gl/1BLDCQV6bJBQWPeu9',
+        image: 'https://photos.app.goo.gl/N1fFZ8gGta95u8mE7',
+    },
+    {
+        name: 'Yellow Zinnias',
+        category: 'traditionalproduce',
+        amount: '1 pound',
+        price: '$1/pound',
+        description: 'Seed from Mayan origin',
+        image: 'https://photos.app.goo.gl/Y31EYYisJxpzeWUU7',
+    },
+    {
+        name: 'Orange Zinnias',
+        category: 'traditionalproduce',
+        amount: '1 pound',
+        price: '$1/pound',
+        description: 'Seed from Mayan origin',
+        image: 'https://photos.app.goo.gl/2Yn9EVxynevRNkGE8',
     }
 ]
 export default flowers
