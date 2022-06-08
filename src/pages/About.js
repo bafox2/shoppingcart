25fe905962096ab68b065711aa7a4c90b14712f7
ben <benjaminalexanderfox11@gmail.com>
2022-06-07 22:53:47 -0400

    new start git is hard

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,8 +7,8 @@ const About = (props) => {
         <div>
             <main>
                 <h1>Welcome to About</h1>
-                <p>This farm was started :</p>
-                <p>This farm uses the techniques :</p>
+                <p>This farm was started : in </p>
+                <p>This farm uses the techniques : the</p>
                 <p>In the future we hope to become :</p>
             </main>
         </div>
