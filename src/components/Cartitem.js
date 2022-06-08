1721298ecb553b09f59b619cf37465b279300342
ben <benjaminalexanderfox11@gmail.com>
2022-06-08 12:59:10 -0400

    added cartitem component

---
+++ src/components/Cartitem.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import styles from "../css/Item.module.css";
+import { Link, use } from "react-router-dom";
+
+//might want to add a category for each individual categorty
+
+const Cartitem = (props) => {
+
+    return (
+        <div role='listitem'>
+            {/* {item} */}
+            <Link to={`/shop/${props.name}`} title='itemLink'>{props.name}</Link>
+            <p>add {props.name} to cart</p>
+            <p>{props.description}</p>
+            <p>{props.amount} for</p>
+            <p>{props.price}</p>
+            <img className={styles.itemPic} alt='item' src={props.picture} />
+        </div>
+    );
+};
+
+export default Cartitem;
--- src/components/Sidecart.js
+++ src/components/Sidecart.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../css/Header.module.css";
-import Item from "./Item";
+import Cartitem from "./Item";
 // state of cart stuff might be handled in the APP itself
 
 const Sidecart = (props) => {
     console.log(props)
-    const cartItems = props.cart.map(item => <Item name={item.name} description={item.quantity} />)
+    const cartItems = props.cart.map(item => <Cartitem name={item.name} description={item.quantity} />)
     return (
         <div >
             <p>this is your cart</p>
