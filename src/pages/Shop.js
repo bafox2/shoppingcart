f31087561bf7633023c2eb67bc3d0377dc8d39ae
ben <benjaminalexanderfox11@gmail.com>
2022-06-13 10:07:10 -0400

    knocked state down a level

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import styles from "../css/Shop.module.css";
 import Item from "../components/Item"
 import Sidebar from "../components/Sidebar";
@@ -6,14 +6,45 @@ import items from "../itemData/allItems.js";
 
 const Shop = (props) => {
     console.log(props)
-
-    const filteredList = items.filter(item => item)
     const shoplist = items.map(item => <Item name={item.name} picture={item.imageref} price={item.price} amount={item.amount} category={item.category} key={item.name} id={item.id} role='item' />)
+    // won't work because the original list has to stay available
+    const [list, setList] = useState([]);
+    const [filterList, setFilterList] = React.useState({
+        produce: true,
+        flowers: true,
+        microgreens: true,
+        sort: 'priceHigh',
+        search: ''
+    })
+
+    const handleFilterChange = (event) => {
+        console.log(event)
+        const { name, value, type, checked } = event.target
+        setFilterList((prevOptions) => {
+            return {
+                ...prevOptions,
+                [name]: type === 'checkbox' ? checked : value,
+            }
+        })
+    }
+
+    const produce = items.filter(item => item.category === 'produce')
+    const filteredList = items.filter(item => item)
+
+
+    function handleSort(props) {
+        const nextList = [...list];
+        if (props.filters.sort === 'priceHigh') {
+            //high to low
+            nextList.sort(function (a, b) { return b.price - a.price });
+        }
+        setList(nextList);
+    }
 
     return (
         <div>
             <h1 title="shopHeader">shop</h1>
-            <Sidebar handleChange={props.handleChange} state={props.filters}></Sidebar>
+            <Sidebar handleChange={handleFilterChange} state={filterList}></Sidebar>
             {shoplist}
         </div>
     );
